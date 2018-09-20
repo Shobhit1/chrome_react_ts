0b1ae9393df967940a2136ea0c7488839bf0eedb
Shobhit Agarwal <shoagarw@microsoft.com>
2018-09-20 16:40:57 -0700

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,10 @@ import * as ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-
+/**
+ * @todo Take over the world
+ * @body Humans are weak; Robots are strong. We must cleans the world of the virus that is humanity.
+ */
 ReactDOM.render(
   <App />,
   document.getElementById('root') as HTMLElement
